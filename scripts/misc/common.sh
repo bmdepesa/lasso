1a761a3cf188dbafe1620d8cb289cbc45c941c9a
Brandon DePesa <brandon@rancher.com>
2019-02-02 09:10:11 -0700

    .gitignore update, removed duplicate functions

--- scripts/misc/common.sh
+++ scripts/misc/common.sh
@@ -16,16 +16,6 @@ function set_kube_config {
   fi
 }
 
-function rancher_get_ha_leader {
-	kubectl get configmaps cattle-controllers -n kube-system -o json | \
-	jq -r '.metadata.annotations["control-plane.alpha.kubernetes.io/leader"]' | \
-	jq -r '.holderIdentity'
-}
-
-function rancher_get_ha_leader_logs {
-	kubectl logs -n cattle-system $(rgl) $1
-}
-
 function lasso_logo {
 	printf "\n"
 	cat << "EOF"
